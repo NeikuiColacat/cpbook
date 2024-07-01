38e9c7c41ea45fbb1abc80de3dd6a0b08501662a
NeikuiColacat <neikui.colacat@gmail.com>
2024-07-01 16:39:27 +0800

    add cal C

---
+++ binomial coefficient.hpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+constexpr int mod = 1e9+7 , N = 1e5 + 10;
+std::vector<int> pw(N+1);
+int fp(int a, int n, int mod) {
+    int res = 1;
+    while (n) {
+        if (n % 2) res = (long long)res * a % mod;
+        a = (long long)a * a % mod;
+        n >>= 1;
+    }
+    return res;
+}
+int inv(int x, int mod = mod) { return fp(x, mod - 2, mod); }
+int C(int a, int b) {
+    if (pw[0] == 0) {
+        pw[0] = 1;
+        for (int i = 1;i <= N;i++) pw[i] = (long long)pw[i - 1] * i % mod;
+    }
+    for (int i = 1;i <= N;i++) pw[i] = (long long)pw[i - 1] * i % mod;
+    if (a < b) return 0;
+    return (long long)pw[a] * inv(pw[b]) % mod * inv(pw[a - b]) % mod;
+}
