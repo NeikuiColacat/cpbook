d918d92957e89a9f5e8e400bf2fbc721d58fab36
NeikuiColacat <neikui.colacat@gmail.com>
2024-07-05 12:23:30 +0800

    change mint

--- mint.hpp
+++ mint.hpp
@@ -1,15 +1,40 @@
+#include <bits/stdc++.h>
 struct mint {
-    const int num , mod = 998244353;
+    int num , mod = 998244353;
     mint(int num) : num(num) {}
     mint(int num, int mod) : num(num), mod(mod) {}
 
-    int operator+(const mint & other) {
+    mint operator+(const mint & other) {
         return ((long long)num + other.num )% mod;
     }
-    int operator-(const mint& other) {
+    mint operator-(const mint& other) {
         return ((long long)num - other.num + mod) % mod;
     }
-    int operator*(const mint& other) {
+    mint operator*(const mint& other) {
         return (long long)num * other.num % mod;
+    }
+    mint operator*=(const mint& other) {
+        return num = (long long)num * other.num % mod;
+    }
+    mint operator+=(const mint & other) {
+        return num = ((long long)num + other.num) % mod;
+    }
+    mint operator-=(const mint& other) {
+        return num = ((long long)num - other.num + mod) % mod;
+    }
+    mint operator=(const int& val) {
+        num = val;
+    }
+    mint operator=(const mint& other) {
+        num = other.num;
+    }
+    friend std::istream& operator>>(std::istream& is, mint& modNumber) {
+        is >> modNumber.num;
+        modNumber.num %= modNumber.mod;
+        return is;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const mint& modNumber) {
+        os << modNumber.num;
+        return os;
     }
 };
