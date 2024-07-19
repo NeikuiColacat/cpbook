29effe1f9faf4070d94a93e40a9d4eff3bfb0981
cola <neikui.colacat@gmail.com>
2024-07-19 18:13:07 +0800

    modify file tree

---
+++ a.cpp
@@ -0,0 +1,27 @@
+/**
+ *  author: NeikuiColacat
+ *  created: 19.07.2024 17:23:50
+ **/
+#include <bits/stdc++.h>
+#include <queue>
+#define fi first
+#define se second
+#define si(x) ((i32)x.size())
+using std::cout, std::cin, std::endl;
+using std::max, std::min, std::swap, std::vector, std::string;
+using i32 = int32_t;
+using i64 = int64_t;
+using P = std::pair<i32, i32>;
+
+void solve() {
+
+}
+
+signed main() {
+    std ::ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    i32 t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
