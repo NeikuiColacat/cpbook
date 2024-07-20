67b11afbe77be608cfaafe580d66b9a03c97cf07
cola <neikui.colacat@gmail.com>
2024-07-20 12:52:27 +0800

    change dsu

--- dsu.hpp
+++ dsu.hpp
@@ -1,7 +1,7 @@
 struct dsu {
     const i32 N;
-    std::vector<i32> p;
-    dsu(i32 siz) : N(siz), p(std::vector<i32>(N + 1)) {
+    std::vector<i32> p , siz;
+    dsu(i32 siz) : N(siz), p(std::vector<i32>(N + 1)) , siz(std::vector<i32>(N+1 , 1)) {
         iota(p.begin()+1,p.end(),1);
     }
 
@@ -11,6 +11,10 @@ struct dsu {
 
     void mer(i32 u, i32 v) {
         u = find(u) , v = find(v);        
-        p[u] = v;
+        if(u != v){
+            if (siz[u] > siz[v]) std::swap(u, v);
+            p[u] = v;
+            siz[v] += siz[u];
+        }
     }
 };
