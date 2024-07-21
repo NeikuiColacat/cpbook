1ebb41c64d5560ba58bea4071fdf1e9dd3096dfe
cola <neikui.colacat@gmail.com>
2024-07-22 00:23:09 +0800

    add init1

---
+++ init1.hpp
@@ -0,0 +1,24 @@
+/**
+ *  author: NeikuiColacat 
+ *  created: $CURRENT_DATE.$CURRENT_MONTH.$CURRENT_YEAR $CURRENT_HOUR:$CURRENT_MINUTE:$CURRENT_SECOND
+**/
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define si(x) ((i32)x.size())
+using std::cout, std::cin, std::endl;
+using std::max, std::min, std::swap, std::vector, std::string;
+using i32 = int32_t;
+using i64 = int64_t;
+using P = std::pair<i32,i32>;
+
+void solve() {
+    ${0}
+}
+
+signed main() {
+    std :: ios::sync_with_stdio(0);
+    cin.tie(0) , cout.tie(0);
+    solve();
+    return 0;
+}  
