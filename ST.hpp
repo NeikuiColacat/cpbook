1255833b55ef13faa5f384802dcd1779597c9813
NeikuiColacat <neikui.colacat@gmail.com>
2024-08-30 21:38:35 +0800

    modify starting

--- ST.hpp
+++ ST.hpp
@@ -1,21 +1,22 @@
+#include <bits/stdc++.h>
+using i32 = int;
 template <typename T, T(*op)(T, T)>
 struct ST {
 	const i32 N;
 	std::vector<std::vector<T>> st;
-	ST(const std::vector<T>& a) : N((i32)a.size() - 1), st(std::vector<std::vector<T>>(N + 1, std::vector<T>(31))){
-		for (i32 i = 1; i <= N; i++) {
-			st[i][0] = a[i];
-		}
+    ST(i32 siz, const std::vector<T>& a)
+        : N(siz), st(std::vector<std::vector<T>>(N + 1, std::vector<T>(31))) {
+        for (i32 i = 1; i <= N; i++) { st[i][0] = a[i]; }
 
-		for (i32 i = 1; i <= std::__lg(N); i++) {
-			for (i32 j = 1; j + (1 << i) - 1 <= N; j++) {
-				st[j][i] = op(st[j][i - 1], st[j + (1 << i - 1)][i - 1]);
-			}
-		}
-	}
+        for (i32 i = 1; i <= std::log2(N); i++) {
+            for (i32 j = 1; j + (1 << i) - 1 <= N; j++) {
+                st[j][i] = op(st[j][i - 1], st[j + (1 << i - 1)][i - 1]);
+            }
+        }
+    }
 
-	T qry(i32 l, i32 r) {
-        i32 lglen = std::__lg(r - l + 1);
+    T qry(i32 l, i32 r) {
+        i32 lglen = std::log2(r - l + 1);
 		return op(st[l][lglen], st[r - (1 << lglen) + 1][lglen]);
 	}
 };
