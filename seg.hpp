e8390da25e344c182071db328fad3f36baceba05
NeikuiColacat <neikui.colacat@gmail.com>
2024-10-26 02:17:42 +0800

    s

--- seg.hpp
+++ seg.hpp
@@ -5,21 +5,26 @@ struct seg {
     vector<T> tr;
     seg(i32 siz, T te) : N(siz), Te(te), tr((N + 10) << 2, Te) {}
     T mer(T a, T b) {
-        // TODO
+
     }
-    void apply(T& a, T b) {
-        // TODO
+    void down(i32 u) {
+
     }
     void up(i32 u) { 
         tr[u] = mer(tr[u << 1], tr[u << 1 | 1]); 
     }
-    void down(i32 u) {
-        apply(tr[u << 1], tr[u]);
-        apply(tr[u << 1 | 1], tr[u]);
+    void build(i32 u , i32 l , i32 r){
+        if(l == r){
+
+        } else{
+            i32 mi = l + r >> 1;
+            build(u<<1,l,mi) , build(u<<1|1,mi+1,r);
+            up(u);
+        }
     }
     void modify(i32 u, i32 l, i32 r, const i32 ql, const i32 qr,T val) {
         if (ql <= l && r <= qr) {
-            apply(tr[u], val);
+
         }
         else {
             down(u);
