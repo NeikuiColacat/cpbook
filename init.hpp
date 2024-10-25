d59b5c0881d27ddffa1970b19b9d64bc1cdf21b5
NeikuiColacat <neikui.colacat@gmail.com>
2024-10-25 23:07:08 +0800

    lueluelue

--- init.hpp
+++ init.hpp
@@ -6,16 +6,9 @@
 #define x first
 #define y second
 #define si(x) ((i32)x.size())
-<<<<<<< HEAD
+#define endl '\n'
 using namespace std;
 using i32 = int;
-=======
-#define endl '\n'
-using std::cout, std::cin, std::cerr;
-using std::max, std::min, std::swap;
-using std::vector, std::string, std::map, std::set, std::queue, std::deque, std::priority_queue;
-using i32 = signed int;
->>>>>>> d219320fbf6305de97aa78c85fd83f9278b1be9f
 using i64 = long long;
 using P = std::pair<i32,i32>;
 template<typename T> void cmax(T & a , T b){a = max(a,b);}
@@ -28,8 +21,7 @@ void solve() {
 signed main() {
     std :: ios::sync_with_stdio(0);
     cin.tie(0) , cout.tie(0);
-    i32 t;
-    cin >> t;
-    while (t--) solve();
+    i32 t;cin>>t;while(t--)
+    solve();
     return 0;
 }  
